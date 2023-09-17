901d8238f4855cf609a927c199e90cc9862165be
Samuel Muli <samuelmuli95@gmail.com>
2023-09-17 10:03:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainTag = document.querySelector('#main')
+mainTag.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Samuel is the champion"
